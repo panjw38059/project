b84bd9b3d2ce2d68fe75622cf28173a9c4058563
panjw <panjunwen0901@163.com>
2022-08-09 22:36:01 +0800

    first commit

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,8 +1,13 @@
 <template>
-  <div>
-    <detail-nav-bar></detail-nav-bar>
-    <detail-swiper :top-images="topImages"></detail-swiper>
-    <detail-base-info :goods = "Goods"></detail-base-info>
+  <div id="detail">
+    <detail-nav-bar class="nav-bar-detail"></detail-nav-bar>
+    <scroll class="content" ref="scroll">
+      <detail-swiper :top-images="topImages"></detail-swiper>
+      <detail-base-info :goods = "Goods"></detail-base-info>
+      <detail-shop-info :shop="Shop"></detail-shop-info>
+      <detail-goods-info :detailInfo = "detailInfo" @imageLoad = "imageLoad"></detail-goods-info>
+      <detail-param-info :paramInfo = "paramsInfo"></detail-param-info>
+    </scroll>
   </div>
 </template>
 
@@ -11,17 +16,24 @@
 import {getDetailData} from "network/detail"
 import DetailNavBar from "./children/DetailNavBar";
 import DetailSwiper from "@/views/detail/children/DetailSwiper";
-import {GoodsInfo} from "@/network/detail";
+import {GoodsInfo, GoodsParam, Shop} from "@/network/detail";
 import DetailBaseInfo from "@/views/detail/children/DetailBaseInfo";
+import DetailShopInfo from "@/views/detail/children/DetailShopInfo";
+import Scroll from "@/components/common/scroll/Scroll";
+import DetailGoodsInfo from "@/views/detail/children/DetailGoodsInfo";
+import DetailParamInfo from "@/views/detail/children/DetailParamInfo";
 
 export default {
   name: "Detail",
-  components: {DetailBaseInfo, DetailSwiper, DetailNavBar},
+  components: {DetailParamInfo, DetailGoodsInfo, Scroll, DetailShopInfo, DetailBaseInfo, DetailSwiper, DetailNavBar},
   data() {
     return {
       iid: null,
       topImages:[],
-      Goods:{}
+      Goods:{},
+      Shop:{},
+      detailInfo:{},
+      paramsInfo:{},
     }
   },
   created() {
@@ -30,11 +42,32 @@ export default {
       const data = res.result
       this.topImages = res.result.itemInfo.topImages
       this.Goods = new GoodsInfo(data.itemInfo,data.columns,data.shopInfo.services)
+      this.Shop = new Shop(data.shopInfo)
+      this.detailInfo =data.detailInfo
+      this.paramsInfo = new GoodsParam(data.itemParams.info,data.itemParams.rule)
     })
+  },
+  methods:{
+    imageLoad(){
+      this.$refs.scroll.refresh()
+    }
   }
 }
 </script>
 
 <style scoped>
-
+#detail{
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+  height: 100vh;
+}
+.content{
+  height: calc(100% - 44px);
+}
+.nav-bar-detail{
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+}
 </style>
--- src/views/detail/children/DetailBaseInfo.vue
+++ src/views/detail/children/DetailBaseInfo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="Object.keys(goods).length() !== 0" class="base-info">
+  <div v-if="Object.keys(goods).length != 0" class="base-info">
     <div class="info-title">{{goods.title}}</div>
     <div class="info-price">
       <span class="n-price">{{goods.newPrice}}</span>
